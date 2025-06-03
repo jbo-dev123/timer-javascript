fc2c007dd2ac5a45b53ed4d11d3f6e9fc77401ff
jbo-dev123 <johnnybragueto@hotmail.com>
2025-06-03 18:28:58 -0300

    adicionei uma função que muda o conteudo do texto quando clica em pausar e zerar

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,7 @@ function criaHoraDosSegundos(segundos) {
 
 }
 
+const botaoIniciar = document.querySelector('.iniciar');
 const relogio = document.querySelector('.relogio');
 let segundos = 0;
 let timer;
@@ -22,25 +23,27 @@ document.addEventListener('click', function (e) {
     const elementoSendoClicado = e.target;
 
     if (elementoSendoClicado.classList.contains('iniciar')) {
-        relogio.classList.remove('pausado');
-        clearInterval(timer)
-        iniciaRelogio();
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    iniciaRelogio();
 
-    }
+   botaoIniciar.textContent = 'Iniciar'
+}
 
     if (elementoSendoClicado.classList.contains('pausar')) {
         relogio.classList.add('pausado');
         clearInterval(timer);
-
+        botaoIniciar.textContent = 'Continuar'
     }
 
     if (elementoSendoClicado.classList.contains('zerar')) {
-        relogio.classList.remove('pausado');
-        clearInterval(timer);
-        relogio.innerHTML = '00:00:00'
-        segundos = 0;
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
 
-    }
+    botaoIniciar.textContent = 'Iniciar';
+}
 
 })
 
